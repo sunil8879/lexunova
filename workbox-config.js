87a9284033de87e869f08ad431567011c370e1b9
sunil8879 <sunilvasarkar@yahoo.com>
2026-06-07 01:42:29 +0530

    Add files via upload

---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    globDirectory: '.',
+    globPatterns: [
+        '**/*.{html,js,css,png,svg,jpg,gif,json}'
+    ],
+    swDest: 'sw-workbox.js',
+ cacheId: 'wellspokn-pwa-v2' 
+};
